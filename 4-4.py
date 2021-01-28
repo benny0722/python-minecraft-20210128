ff93dd2d9f466f83696ad8303bb3747d9708e99e
benny0722 <77959759+benny0722@users.noreply.github.com>
2021-01-28 13:48:03 +0800

    Add files via upload

---
+++ 4-4.py
@@ -0,0 +1,18 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+list2 = [[12,41,14],[35,73,86]]
+
+myID = mc.getPlayerEntityId("APE_42")
+x,y,z = mc.entity.getTilePos(myID)
+
+startX = x
+
+for i in list2:
+    for j in i:
+        
+        mc.setBlock(x,y,z,j)
+        x = x+1
+    x = startX
+    z = z-1
+
